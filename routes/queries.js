d7a4303fa9f2acd1860d9c4ab2e6c46cb64b77be
Krishna Dhakshin K <krishnadhakshin2000@gmail.com>
2022-03-31 22:18:35 +0530

    reusable views

--- routes/queries.js
+++ routes/queries.js
@@ -1,7 +1,7 @@
 module.exports = {
     query1: (req, res) => {
         console.log("Q1")
-        res.render('query1.ejs', {})
+        res.render('query_compare.ejs', {p1: "Batter", p2: "Bowler", n: 1})
     },
     query2: (req, res) => {
         console.log("Q2")
@@ -13,14 +13,30 @@ module.exports = {
     },
     query4: (req, res) => {
         console.log("Q4")
-        res.render('query4.ejs', {})
+        res.render('query_compare.ejs', {p1: "Player-1", p2: "Player-2", n: 4})
     },
     query5: (req, res) => {
         console.log("Q5")
-        res.send('Q5')
+        res.render('query_team.ejs', {n: 5})
     },
     query6: (req, res) => {
         console.log("Q6")
-        res.send('Q6')
-    }
+        res.render('query_team.ejs', {n: 6})
+    },
+    query7: (req, res) => {
+        console.log("Q7")
+        res.render('query_player.ejs', {n: 7, type: 'Batter'})
+    },
+    query8: (req, res) => {
+        console.log("Q8")
+        res.render('query_player.ejs', {n: 8, type: 'Batter'})
+    },
+    query9: (req, res) => {
+        console.log("Q9")
+        res.render('query_player.ejs', {n: 9, type: 'Bowler'})
+    },
+    query10: (req, res) => {
+        console.log("Q10")
+        res.render('query_player.ejs', {n: 10, type: 'Bowler'})
+    },
 }
--- routes/responses.js
+++ routes/responses.js
@@ -1,9 +1,9 @@
 const { dBPool } = require('../services/db')
 module.exports = {
     searchPlayers: (req, res) => {
-        console.log(req.query)
+        // console.log(req.query)
         dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
-            console.log({results, fields})
+            // console.log({results, fields})
             res.json(results)
         })
     },
@@ -11,14 +11,20 @@ module.exports = {
         console.log(req.query)
         dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
             console.log({results, fields})
+            results = [{team_id: 1, team_name: "RR"}, {team_id: 2, team_name: "KKR"}]
             res.json(results)
         })
     },
     response1: (req, res) => {
         dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
-            console.log({results})
-            stats = results[0]
-            res.render('response1.ejs', {stats, batsman: "Batter", bowler: "Bowler"});
+            console.log({results}, req.body)
+            results = [{Season: 2011, Batter: "Kohli", Bowler: "Bumrah", Wickets: 3, Runs: 220}, {Season: 2023, Batter: "Kohli", Bowler: "Bumrah", Wickets: 2, Runs: 520}]
+            const stats = results.map((result) => {
+                const { Batter, Bowler, ...stat } = result
+                return stat
+            })
+            console.log({stats})
+            res.render('response_compare.ejs', {stats, p1: results[0]['Batter'] + " (Batter)", p2: results[0]['Bowler'] + " (Bowler)"});
         })
     },
     response2: (req, res) => {
@@ -38,8 +44,86 @@ module.exports = {
     response4: (req, res) => {
         dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
             console.log({results}, req.body)
-            stats = results[0]
-            res.render('response4.ejs', {stats, player1: "Player1", player2: "Player2"});
+            results = [{Season: 2011, Player1: "Kohli", Player2: "Bumrah", Wickets: 3, Runs: 220}, {Season: 2023, Player1: "Kohli", Player2: "Bumrah", Wickets: 2, Runs: 520}]
+            const stats = results.map((result) => {
+                const { Player1, Player2, ...stat } = result
+                return stat
+            })
+            console.log({stats})
+            res.render('response_compare.ejs', {stats, p1: results[0]['Player1'], p2: results[0]['Player2']});
         })
-    }
+    },
+    response5: (req, res) => {
+        dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
+            console.log({results}, req.body)
+            results = [{Team: "RCB", Season: 2011, "Matches Played": 23, "Matches Won": 12}, {Team: "RCB", Season: 2015, "Matches Played": 15, "Matches Won": 13}]
+            const stats = results.map((result) => {
+                const { Team, ...stat } = result
+                return stat
+            })
+            console.log({stats})
+            res.render('response_stats.ejs', {stats, name: results[0]['Team']});
+        })
+    },
+    response6: (req, res) => {
+        console.log(req.query)
+        dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
+            console.log({results})
+            results = [{Season: 2011, Opponent: "RR", Team: "RCB", IsWin: "Yes"}, {Season: 2011, Opponent: "MI", Team: "RCB", IsWin: "No"}, {Season: 2022, Opponent: "KXIP", Team: "RCB", IsWin: "Yes"}]
+            fields = [{name: "Season"}, {name: "Opponent"}, {name: "Team"}, {name: "IsWin"}]
+            const stats = results.map((result) => {
+                const { Team, ...stat } = result
+                return stat
+            })
+            res.render('response_table.ejs', {name: results[0]['Team'], stats, headers: fields.map((field) => field.name).filter(name => name != "Team")});
+        })
+    },
+    response7: (req, res) => {
+        dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
+            console.log({results}, req.body)
+            results = [{Player: "Kohli", Season: 2011, "Runs": 451, "Dismissals": 12}, {Player: "Kohli", Season: 2014, "Runs": 987, "Dismissals": 7}]
+            const stats = results.map((result) => {
+                const { Player, ...stat } = result
+                return stat
+            })
+            console.log({stats})
+            res.render('response_stats.ejs', {stats, name: results[0]['Player']});
+        })
+    },
+    response8: (req, res) => {
+        dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
+            console.log({results})
+            results = [{Season: 2011, Runs: 55, Team: "RCB", MOTM: "Yes", "Player": "Kohli"}, {Season: 2011, Runs: 21, Team: "RCB", MOTM: "No", "Player": "Kohli"}, {Season: 2013, Runs: 34, Team: "KKR", MOTM: "No", "Player": "Kohli"}]
+            fields = [{name: "Season"}, {name: "Runs"}, {name: "Team"}, {name: "MOTM"}, {name: "Player"}]
+            const stats = results.map((result) => {
+                const { Player, ...stat } = result
+                return stat
+            })
+            res.render('response_table.ejs', {name: results[0]['Player'], stats, headers: fields.map((field) => field.name).filter(name => name != "Player")});
+        })
+    },
+    response9: (req, res) => {
+        dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
+            console.log({results}, req.body)
+            results = [{Player: "Bumrah", Season: 2011, "Wickets": 24, "Runs Conceded": 450}, {Player: "Bumrah", Season: 2014, "Runs Conceded": 311, "Wickets": 21}]
+            const stats = results.map((result) => {
+                const { Player, ...stat } = result
+                return stat
+            })
+            console.log({stats})
+            res.render('response_stats.ejs', {stats, name: results[0]['Player']});
+        })
+    },
+    response10: (req, res) => {
+        dBPool.query('SELECT * FROM Sample', (error, results, fields) => {
+            console.log({results})
+            results = [{Season: 2011, Wickets: 3, Team: "RCB", MOTM: "Yes", "Player": "Bumrah"}, {Season: 2011, Wickets: 0, Team: "RCB", MOTM: "No", "Player": "Bumrah"}, {Season: 2013, Wickets: 4, Team: "KKR", MOTM: "No", "Player": "Bumrah"}]
+            fields = [{name: "Season"}, {name: "Wickets"}, {name: "Team"}, {name: "MOTM"}, {name: "Player"}]
+            const stats = results.map((result) => {
+                const { Player, ...stat } = result
+                return stat
+            })
+            res.render('response_table.ejs', {name: results[0]['Player'], stats, headers: fields.map((field) => field.name).filter(name => name != "Player")});
+        })
+    },
 }
